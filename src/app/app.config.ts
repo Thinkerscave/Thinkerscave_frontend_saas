2a86d67998c1f00f7204a11967b1c43a3d064e87
Soumyaranjan-25 <soumyaranjanmahapatra25@gmail.com>
2025-07-06 22:24:40 +0530

    Added auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,17 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptorFn } from './core/interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-   
+    provideHttpClient(
+      withInterceptors([authInterceptorFn])
+    ),
     providePrimeNG({
       theme: {
         preset: Aura
---
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { LoginService } from '../../services/login.service';
+
+export const authInterceptorFn: HttpInterceptorFn = (req, next) => {
+  const loginService = inject(LoginService);
+  const token = loginService.getToken();
+
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` },
+    });
+    return next(authReq);
+  }
+
+  return next(req);
+};
